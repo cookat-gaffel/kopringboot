4bf2957187597eadd71c5d1ef6df335c15ea4aeb
gaffel <gaffel@ip-192-168-10-17.ap-northeast-2.compute.internal>
2024-03-14 09:29:23 +0900

    add RestAPI HelloController

--- src/main/kotlin/cookat/study/kopringboot/HelloController.kt
+++ src/main/kotlin/cookat/study/kopringboot/HelloController.kt
@@ -1,4 +1,13 @@
 package cookat.study.kopringboot
 
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("api/hello")
 class HelloController {
+
+    @GetMapping
+    fun hello(): String = "Hello Kotlin + SpringBoot! this is a REST Endpoint"
 }
